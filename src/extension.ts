fbf6ed26d66e697d1d2eada203bbeebe103c74fd
Chris Griffing <cmgriffing@gmail.com>
2020-09-27 14:06:33 -0700

    fix: update broken import

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import {
   languages,
   ExtensionContext,
 } from "vscode";
-export { activate } from "./CodelensProvider";
+export { activate } from "./activate";
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
